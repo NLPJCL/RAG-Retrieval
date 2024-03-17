dd8f3d065d19f69ae42c82095197ad672f947556
grok <42383434+NLPJCL@users.noreply.github.com>
2024-03-18 01:29:52 +0800

    Update train_embedding.sh

--- rag-retrieval/embedding/train_embedding.sh
+++ rag-retrieval/embedding/train_embedding.sh
@@ -15,11 +15,11 @@ CUDA_VISIBLE_DEVICES="0"   nohup  accelerate launch --config_file ../../config/d
 --model_name_or_path "BAAI/bge-base-zh-v1.5" \
 --dataset "../../example_data/t2rank_100.json" \
 --output_dir "./output/t2ranking_100_example" \
---batch_size 8 \
---lr 5e-5 \
+--batch_size 4 \
+--lr 2e-5 \
 --epochs 2 \
 --save_on_epoch_end 1 \
---gradient_accumulation_steps 12  \
+--gradient_accumulation_steps 24  \
 --log_with 'wandb' \
 --warmup_proportion 0.1 \
 --neg_nums 15 \
