2f8409c985039336f00a8d7846a561b343462d68
grok <42383434+NLPJCL@users.noreply.github.com>
2024-12-12 01:13:46 +0800

    Update train_embedding.sh

--- rag_retrieval/train/embedding/train_embedding.sh
+++ rag_retrieval/train/embedding/train_embedding.sh
@@ -10,6 +10,7 @@ if [ ! -d "./logs" ]; then
     echo "mkdir logs"
 fi
 
+#bert-based embedding (without mrl)
 CUDA_VISIBLE_DEVICES="0"   nohup  accelerate launch --config_file ../../../config/default_fsdp.yaml train_embedding.py  \
 --model_name_or_path "BAAI/bge-base-zh-v1.5" \
 --dataset "../../../example_data/t2rank_100.jsonl" \
@@ -27,7 +28,7 @@ CUDA_VISIBLE_DEVICES="0"   nohup  accelerate launch --config_file ../../../confi
 --passage_max_len 512 \
  >./logs/t2ranking_100_example.log &
 
-
+#bert-based embedding (with mrl)
 #  CUDA_VISIBLE_DEVICES="0"   nohup  accelerate launch --config_file ../../../config/default_fsdp.yaml train_embedding.py  \
 # --model_name_or_path "BAAI/bge-base-zh-v1.5" \
 # --dataset "../../../example_data/t2rank_100.jsonl" \
@@ -46,3 +47,21 @@ CUDA_VISIBLE_DEVICES="0"   nohup  accelerate launch --config_file ../../../confi
 # --query_max_len 128 \
 # --passage_max_len 512 \
 #  >./logs/t2ranking_100_example_mrl1792.log &
+
+#llm-based embedding
+# CUDA_VISIBLE_DEVICES="0,1,2,3"   nohup  accelerate launch --config_file ../../../config/deepspeed/deepspeed_zero2.yaml train_embedding.py  \
+# --model_name_or_path "Alibaba-NLP/gte-Qwen2-7B-instruct" \
+# --dataset "../../../example_data/t2rank_100.jsonl" \
+# --output_dir "./output/t2ranking_100_example" \
+# --batch_size 4 \
+# --lr 2e-5 \
+# --epochs 2 \
+# --save_on_epoch_end 1 \
+# --gradient_accumulation_steps 24  \
+# --log_with 'wandb' \
+# --warmup_proportion 0.05 \
+# --neg_nums 15 \
+# --temperature 0.02 \
+# --query_max_len 256 \
+# --passage_max_len 1024 \
+#  >./logs/t2ranking_100_example.log &
