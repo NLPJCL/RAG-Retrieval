63dfa85f13aae519327b077129cd7c19f767b57c
jcli <953977230@qq.com>
2025-01-04 16:08:51 +0800

    update stella_embedding create_distill_data

--- examples/stella_embedding/README.md
+++ examples/stella_embedding/README.md
@@ -1,21 +1,31 @@
 
+Refer to create_distill_data.sh to extract the embeddings of teacher1 and teacher2 in turn.
 
-#Use the following command to extract the embedding of the teacher model from the query, pos, and neg of train_data_path, and then output it to distill_train_data_path.
-```bash
+Tips:
+- If the embedding model you want to distill requires instructions, please modify the create_distill_data.py to add them.
+- For create_distill_data.py input file: train_data_path, No need to explain query, pos and neg are the doc texts to be distilled.
+There are two options,  
+  1. They can be doc text that is irrelevant to the query.
+  2. They can be positive and hard negative examples of the query. If this is the case, and there is no shuffling during training, the distillation effect will be slightly better.
 
-train_data_path="../../example_data/t2rank_100.jsonl"
-ckpt_path="Alibaba-NLP/gte-Qwen2-7B-instruct"
-distill_train_data_path="../../../example_data/t2rank_100.embedding"
-nohup python   create_distill_data.py $train_data_path $ckpt_path $distill_train_data_path >./distill.log &
-
-```
-
-- train_data_path: refer to[example_data](https://github.com/NLPJCL/RAG-Retrieval/blob/master/example_data/t2rank_100.jsonl)的jsonl文件。
+input:
+- train_data_path: refer to[example_data](https://github.com/NLPJCL/RAG-Retrieval/blob/master/example_data/t2rank_100.jsonl).
 ```
 {"query": str, "pos": List[str], "neg":List[str]}
 ```
-- t2rank_100.embedding.mmap:  np.memmap(teacher embedding)
-- t2rank_100.embedding.text.jsonl: {"query}
+
+output:
+- t2rank_100.embedding.model_name.mmap: np.memmap(teacher embedding)
+- t2rank_100.jsonl.text.jsonl: (If the embedding model you are training requires instructions for query, you can add the prompt_for_query field.)
+```
+{"query": str, "prompt_for_query"(optional): str}
+```
+
+For teachers who want to distill two teachers, they can be concat through concate_two_teacher_embedding.sh
+
+
+
+
 
 
 
--- examples/stella_embedding/create_distill_data.py
+++ examples/stella_embedding/create_distill_data.py
@@ -9,17 +9,7 @@ from tqdm.autonotebook import trange
 import torch
 import numpy as np
 
-
-
 from sentence_transformers import SentenceTransformer
-
-
-def get_detailed_instruct(task_description: str, query: str) -> str:
-    return f'Instruct: {task_description}\nQuery: {query}'
-
-
-# Each query must come with a one-sentence instruction that describes the task
-task = 'Given a web search query, retrieve relevant passages that answer the query'
 
 def get_train_data(train_data_path):
     train_data = []
@@ -28,15 +18,13 @@ def get_train_data(train_data_path):
     with open(train_data_path) as f:
         for line in tqdm.tqdm(f):
             data_dic=json.loads(line.strip())
-            
-            detiled_instruct = get_detailed_instruct(task,data_dic['query'])
-            # print(detiled_instruct)
-            if detiled_instruct not in dic:
-                train_data.append(detiled_instruct)
+            query = data_dic['query']
+
+            #Please note whether you need to add instructions to the distilled embedding model?
+            if query not in dic:
+                train_data.append(query)
                 train_data_text.append(data_dic['query'])
-                dic.add(detiled_instruct)
-
-
+                dic.add(query)
             if 'pos' in data_dic:
                 for text_pos in data_dic['pos']:
                     if text_pos not in dic :
@@ -44,19 +32,12 @@ def get_train_data(train_data_path):
                         train_data_text.append(text_pos)
                         dic.add(text_pos)
 
-
-
-
             if 'neg' in data_dic:
                 for text_neg in data_dic['neg']:
                     if text_neg not in dic :
                         train_data.append(text_neg)
                         train_data_text.append(text_neg)
                         dic.add(text_neg)
-
-
-
-
     print(len(train_data))
     print(len(dic))
     return train_data,train_data_text
@@ -64,53 +45,57 @@ def get_train_data(train_data_path):
 
 if __name__=="__main__":
 
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         print(
-            "Usage: python create_distill_data.py [train_data_path] [ckpt_path] [distill_train_data_path]"
+            "Usage: python create_distill_data.py [train_data_path] [ckpt_path] [distill_train_data_path] [save_text]"
         )
         sys.exit(0)
 
     input_train_data_path = sys.argv[1]
     ckpt_path = sys.argv[2]
     distill_train_data_path = sys.argv[3]
+    save_text = sys.argv[4]
+
 
     train_data, train_data_text = get_train_data(input_train_data_path)
     
-    #https://github.com/UKPLab/sentence-transformers/blob/52162f3f0b984a2032667a2332bf95f6aabcf033/sentence_transformers/SentenceTransformer.py#L444C14-L445C1
     embedding_model = SentenceTransformer(
             ckpt_path,
             trust_remote_code=True,
-            device="cuda:7",
             model_kwargs={
-                "torch_dtype": torch.bfloat16,  # fp16 容易计算出nan
+                "torch_dtype": torch.bfloat16,
             }
         )
 
-    embedding_model.max_seq_length = 1024
+    multi_process_pool = embedding_model.start_multi_process_pool()
 
+    embedding_model.max_seq_length = 512
 
-    train_data_embedding = embedding_model.encode(train_data,
-        batch_size=256,
-        show_progress_bar=True,
-        normalize_embeddings=True,
+    train_data_embedding_dim = embedding_model.get_sentence_embedding_dimension()
+
+    mmap_array = np.memmap(distill_train_data_path, dtype='float32', mode='w+', shape=(len(train_data),train_data_embedding_dim))
+
+    if save_text:
+        f_w = open(input_train_data_path+'.text.jsonl','w')
+
+    batch_size=100000
+    for start_index in trange(0, len(train_data), batch_size, desc=""):
+        batch_train_data = train_data[start_index : start_index + batch_size]
+
+        train_data_embedding = embedding_model.encode_multi_process(batch_train_data,
+            batch_size=256,
+            pool=multi_process_pool,
+            show_progress_bar=True,
+            normalize_embeddings=True,
         )
-    print(train_data_embedding.shape)
+        mmap_array[start_index : start_index + batch_size] = train_data_embedding
+        mmap_array.flush()
 
-    train_data_embedding = train_data_embedding.astype(dtype=np.float32)
-    print(train_data[0])
-    print(train_data_embedding[0])
-    mmap_array = np.memmap(distill_train_data_path+'.mmap', dtype='float32', mode='w+', shape=(len(train_data),len(train_data_embedding[0])))
-
-    batch_size=10000
-    for start_index in trange(0, len(train_data), batch_size, desc="saving mmap_array..."):
-        train_data_embedding_batch = train_data_embedding[start_index : start_index + batch_size]
-        mmap_array[start_index : start_index + batch_size] = train_data_embedding_batch
-
-    
-    with open(distill_train_data_path+'.text.jsonl','w') as f_w:
-
-        for idx, text in enumerate(train_data_text):
-            dic = {}
-            dic['query'] = text
-            f_w.write(json.dumps(dic,ensure_ascii=False)+'\n')    
-    mmap_array.flush()
+        if save_text:
+            batch_train_data_text = train_data_text[start_index : start_index + batch_size]
+            for query in batch_train_data_text:
+                dic = {}
+                dic['query'] = query
+                f_w.write(json.dumps(dic,ensure_ascii=False)+'\n')
+        f_w.flush()
+    embedding_model.stop_multi_process_pool(multi_process_pool)
